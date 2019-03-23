c72737eaca7d33c15d38ef248fcded479d5a2e32
rubyleehs <rubyleehs@live.com>
2019-03-23 19:39:02 +0800

    DCP_08: Count unival binary tree subtrees

--- DCP_08.cs
+++ DCP_08.cs
@@ -5,6 +5,9 @@ namespace DCP_08
     //A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
     //Given the root to a binary tree, count the number of unival subtrees
 
+    //This explanation was totatally not sufficient, had to recode slightly due to misunderstanding
+    //all nodes under it had the same value AS IT.
+
     public class Node
     {
         public string val;
