466b89d4de9fe800989e1c10652bdc5a3ffcd346
rubyleehs <rubyleehs@live.com>
2019-07-14 01:25:23 +0800

    Create DCP_61.cs

---
+++ DCP_61.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DCP_61
+{
+    /*
+    Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+    Do this faster than the naive method of repeated multiplication.
+    For example, pow(2, 10) should return 1024.
+    */
+
+    public class DCP_61
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Pow(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine())));
+        }
+
+
+        public static float Pow(int x, int exponent)
+        {
+            if (exponent < 0) return (1 / Pow(x, -exponent));
+            if (exponent == 0) return 1;
+            if (exponent == 1) return x;
+            if (exponent % 2 == 0) return Pow(x * x, exponent / 2);
+            else return (Pow(x * x, (exponent - 1) / 2) * x);
+        }
+    }
+}
