5258422357a86e6a170a3d62c5b15b1b01ab6aca
rubyleehs <rubyleehs@live.com>
2019-07-23 20:18:49 +0800

    Create DCP_71.cs

---
+++ DCP_71.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace DCP_71
+{
+    /*
+    Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+    */
+
+    //this is a duplicate question?
+
+    public class DCP_71
+    {
+        public static int Rand7()
+        {
+            return RNGCSP.RollDice(7);
+        }
+
+        public static int Rand5()
+        {
+            int output = Rand7();
+            while (output > 5) output = Rand7();
+            return output;
+        }
+    }
+
+    class RNGCSP
+    {
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+        public static byte RollDice(byte numberSides)
+        {
+            if (numberSides <= 0)
+                throw new ArgumentOutOfRangeException("numberSides");
+            byte[] randomNumber = new byte[1];
+            do
+            {
+                rngCsp.GetBytes(randomNumber);
+            }
+            while (!IsFairRoll(randomNumber[0], numberSides));
+            return (byte)((randomNumber[0] % numberSides) + 1);
+        }
+
+        private static bool IsFairRoll(byte roll, byte numSides)
+        {
+            int fullSetsOfValues = Byte.MaxValue / numSides;
+            return roll < numSides * fullSetsOfValues;
+        }
+    }
+}
