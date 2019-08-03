511cadc82a81926e36d09343cf8e4e6994cc7724
rubyleehs <rubyleehs@live.com>
2019-08-03 23:32:23 +0800

    Create DCP_85.cs

---
+++ DCP_85.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DCP_85
+{
+    /*
+    Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, using only mathematical or bit operations. You can assume b can only be 1 or 0.
+    */
+
+    public class DCP_85
+    {
+        public static void Main()
+        {
+            Console.WriteLine(F(10,20,0) == 20);
+            Console.WriteLine(F(10, 20, 1) == 10);
+        }
+
+        public static int F(int x, int y, int b)
+        {
+            return x * b + y * Math.Abs(b - 1);
+        }
+    }
+}
