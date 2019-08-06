e6c9a103bfd680b1ef22cac7935ab1bce1e0f230
rubyleehs <rubyleehs@live.com>
2019-08-06 23:48:07 +0800

    Create DCP_88.cs

---
+++ DCP_88.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DCP_88
+{
+    /*
+        Implement division of two positive integers without using the division, multiplication, or modulus operators. Return the quotient as an integer, ignoring the remainder.
+    */
+
+    //There is the obvious solution of just minus till you cant i guess
+    //also bit munipulation. 
+
+    public class DCP_88
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine(BruteDivide(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+            }
+        }
+        public static int BruteDivide(int dividend, int divisor)
+        {
+            int output = 0;
+            while (dividend > divisor)
+            {
+                dividend -= divisor;
+                output++;
+            }
+            return output;
+        }
+    }
+}
