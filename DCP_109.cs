185c43abf8b44bdb3f41096a78ca08b6feac4703
rubyleehs <rubyleehs@live.com>
2019-08-25 00:17:18 +0800

    Create DCP_109.cs

---
+++ DCP_109.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DCP_109
+{
+    /*
+    Given an unsigned 8-bit integer, swap its even and odd bits. The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.
+    For example, 10101010 should be 01010101. 11100010 should be 11010001.
+
+    Bonus: Can you do this in one line?
+    */
+    public class DCP_109
+    {
+        public static byte SwapBits(byte input)
+        {
+            return ((num & 85) << 1) | ((num & (85 << 1)) >> 1);
+        }
+    }
+}
