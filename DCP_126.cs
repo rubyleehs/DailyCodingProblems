b2377b2a8109032198b10ba3c18d513743d13700
rubyleehs <rubyleehs@live.com>
2019-09-14 22:13:45 +0800

    Create DCP_126.cs

---
+++ DCP_126.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DCP_126
+{
+    /*
+    Write a function that rotates a list by k elements. For example, [1, 2, 3, 4, 5, 6] rotated by two becomes [3, 4, 5, 6, 1, 2]. 
+    Try solving this without creating a copy of the list. How many swap or move operations do you need?
+    */
+
+    //This is trivial if it's a C# List
+
+    public class DCP_126
+    {
+        public static void RotateList(List<T> l, int k)
+        {
+            l.AddRange(l.GetRange(0, k));
+            l.RemoveRange(0, k);
+        }
+    }
+}
