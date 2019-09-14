82bb4e60f7104300b3348079d7e63e4bf9a99723
rubyleehs <rubyleehs@live.com>
2019-09-14 22:13:54 +0800

    Create DCP_129.cs

---
+++ DCP_129.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DCP_129
+{
+    /*
+    Given a real number n, find the square root of n. For example, given n = 9, return 3.
+    */
+
+    public class DCP_129
+    {
+        public static void Main()
+        {
+            Console.WriteLine(SquareRoot(9));
+        }
+
+        public static float SquareRoot(int number)
+        {
+            float precision = 0.0001f;
+            float min = 0;
+            float max = number;
+            float result = 0;
+            while (max - min > precision)
+            {
+                result = (min + max) / 2;
+                if ((result * result) >= number)
+                {
+                    max = result;
+                }
+                else
+                {
+                    min = result;
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
