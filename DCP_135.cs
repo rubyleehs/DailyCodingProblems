a735b40cfd5ba53083629c161b93e791f802f113
rubyleehs <rubyleehs@live.com>
2019-09-22 02:48:00 +0800

    Create DCP_135.cs

---
+++ DCP_135.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DCP_135
+{
+    /*
+    Given a binary tree, find a minimum path sum from root to a leaf.
+    For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
+      10
+     /  \
+    5    5
+     \     \
+       2    1
+           /
+         -1
+
+        */
+
+    public class DCP_135
+    {
+    }
+
+    public class Node
+    {
+        Node left;
+        Node right;
+        int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+        public int CalMinPathSumToLeaf()
+        {
+            if (left != null)
+            {
+                if (right != null) return data + Math.Max(right.CalMinPathSumToLeaf(), left.CalMinPathSumToLeaf);
+                return data + left.CalMinPathSumToLeaf(); 
+            }
+            else if(right != null) return data + right.CalMinPathSumToLeaf();
+            return data;
+        }
+    }
+}
