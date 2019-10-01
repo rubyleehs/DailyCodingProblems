400dcf74f0bbfb0183ef737a3e1005355b291e8a
rubyleehs <rubyleehs@live.com>
2019-10-01 12:34:21 +0100

    Create DCP_142.cs

---
+++ DCP_142.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DCP_142
+{
+    /*
+    You're given a string consisting solely of (, ), and *. * can represent either a (, ), or an empty string. Determine whether the parentheses are balanced.
+    For example, (()* and (*) are balanced. )*( is not balanced.
+    */
+
+    public class DCP_142
+    {
+        public static void Main()
+        {
+            Console.WriteLine(isBalance("(()*"));
+            Console.WriteLine(isBalance("(*)"));
+            Console.WriteLine(isBalance(")*(") == false);
+
+        }
+
+        public static bool isBalance(string s, int numOpen = 0)
+        {
+            int o = numOpen;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (o < 0) return false;
+                if (s[i] == '(') o++;
+                else if (s[i] == ')') o--;
+                else if (s[i] == '*')
+                {
+                    if (i == s.Length - 1) return (o == 1);
+                    else if (isBalance(s.Substring(i + 1), o + 1)) return true;
+                    else if (isBalance(s.Substring(i + 1), o - 1)) return true;
+                }
+            }
+
+            return (o == 0);
+        }
+    }
+}
